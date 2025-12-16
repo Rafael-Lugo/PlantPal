c7b0ab631bc72617ea38944ea8dff756f8a6e2ed
Rafael Lugo <rafaelugo@gmx.de>
2025-12-16 16:22:26 +0100

    database update

--- db/models/Plant.js
+++ db/models/Plant.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const plantSchema = new Schema({
-    plant: { type: String, require: true },
+  name: { type: String, require: true },
 });
 
 const Plant = mongoose.models.Plant || mongoose.model("Plant", plantSchema);
--- pages/api/plants/index.js
+++ pages/api/plants/index.js
@@ -7,7 +7,7 @@ export default async function handler(request, response) {
   if (request.method === "GET") {
     const plants = await Plant.find();
     return response.status(200).json(plants);
-  } else {
+  } 
     return response.status(405).json({ message: "Method not allowed" });
-  }
+  
 }
