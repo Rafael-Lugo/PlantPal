c9670a690370eb2273a43342eca3a1b5e20e1ec3
Rafael Lugo <rafaelugo@gmx.de>
2025-12-17 18:15:10 +0100

    dynamic route update

--- pages/plants/[id].js
+++ pages/plants/[id].js
@@ -5,24 +5,29 @@ export default function MyPlantsPage() {
   const router = useRouter();
   const { id } = router.query;
 
-  const { data: plant, isLoading } = useSWR(`/api/plants/${id}`);
+  const { data: plant, isLoading } = useSWR(id ? `/api/plants/${id}` : null);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
 
-  if(!plant){
-    return(
-        <div>
-            <h1>Not found</h1>
-            <p>Plant not found.</p>
-        </div>
+  if (!plant) {
+    return (
+      <div>
+        <h1>Not found</h1>
+        <p>Plant not found.</p>
+      </div>
     );
   }
 
   return (
-    <div>
-      <p>Plant {data.name}</p>
-    </div>
+    <main>
+      <h1>{plant.name}</h1>
+      <p>{plant.botanicalName}</p>
+      <ul>
+        <li>Water: {plant.waterNeed}</li>
+        <li>Light: {plant.lightNeed}</li>
+      </ul>
+    </main>
   );
 }
