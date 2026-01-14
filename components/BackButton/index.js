4b643c0647675823dd01f4acfbb4c23ca8552b84
Rafael Lugo <rafaelugo@gmx.de>
2026-01-14 15:26:29 +0100

    back button style

--- components/BackButton/index.js
+++ components/BackButton/index.js
@@ -1,26 +1,48 @@
 import { useRouter } from "next/navigation";
 import styled from "styled-components";
 
+import Back from "/public/assets/icons/back.svg"
+
 export default function BackButton() {
   const router = useRouter();
 
   return (
-    <StyledBackButton type="button" onClick={() => router.back()}>
-      Back
+    <StyledBackButton type="button" onClick={() => router.back()} aria-label="back button">
+      <Back alt="back" width={48} height={48}/>
     </StyledBackButton>
   );
 }
 
 const StyledBackButton = styled.button`
-  background-color: var(--primary);
-  color: var(--background-foreground);
   border: none;
   cursor: pointer;
-  display: inline-grid;
-  justify-content: end;
+  display: block;
+  align-content: center;
+  align-items: center;
+  margin: 10px;
+
+  height: 60px;
+  width: 60px;
+  border-radius: 50px;
+
+  color: var(--background-foreground);
+  background-color: var(--primary);
+
+  transform: translateY(0);
+  transition: transform 0.3s ease;
 
   &:hover {
     background-color: var(--accent);
     color: var(--color);
+
+    box-shadow: 0px 8px 7px rgba(0, 0, 0, 0.25);
+    transform: translateY(-2px);
+    transition: transform 0.3s ease;
+  }
+
+  &:active {
+    transform: translateY(2px);
+    box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.33);
+    transition: transform 0.3s ease;
   }
 `;
